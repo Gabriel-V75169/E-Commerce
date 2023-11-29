10482c5816386025c99d61712a6f63a28518f2f7
Gabriel Valencia <jrjr75169@gmail.com>
2023-11-29 15:20:34 -0800

    finishing product routes starting tag-routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -35,23 +35,43 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
     const productData = await Product.update(req.body, {
       where: {
         id: req.params.id,
       },
-  })}
+  })
+  if (!productData) {
+    res.status(404).json({ message: 'No product found with this id!' });
+    return;
+  }
+
+  res.status(200).json(productData);
+} 
+catch (err) {
+  res.status(500).json(err);
+}
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   try {
     const productData = await Product.destroy({
       where: {
         id: req.params.id,
       },
   })
+  if (!productData) {
+    res.status(404).json({ message: 'No product found with this id!' });
+    return;
   }
+
+  res.status(200).json(productData);
+} 
+catch (err) {
+  res.status(500).json(err);
+}
 });
 
 module.exports = router;
